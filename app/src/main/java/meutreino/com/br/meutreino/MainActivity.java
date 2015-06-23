868b4c151061b98646ee57978a0407a4c0fec3ca
Thiago Marcello <thiagocmarcello@gmail.com>
2015-06-23 00:56:14 -0300

    Versão final.

--- app/src/main/java/meutreino/com/br/meutreino/MainActivity.java
+++ app/src/main/java/meutreino/com/br/meutreino/MainActivity.java
@@ -1,6 +1,9 @@
 package meutreino.com.br.meutreino;
 
 import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -8,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,8 +23,25 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import meutreino.com.br.meutreino.br.com.meutreino.fragment.ConfiguracaoFragment;
 import meutreino.com.br.meutreino.br.com.meutreino.fragment.SobreFragment;
+import meutreino.com.br.meutreino.dao.USuarioHelper;
+import meutreino.com.br.meutreino.entidade.Treino;
 
 
 public class MainActivity extends ActionBarActivity
@@ -35,12 +56,12 @@ public class MainActivity extends ActionBarActivity
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
-
+    private TextView treinoTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        buscarTreino();
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
@@ -56,6 +77,7 @@ public class MainActivity extends ActionBarActivity
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
         if (position == 0) {
+            buscarTreino();
             fragmentManager.beginTransaction()
                     .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
                     .commit();
@@ -157,6 +179,128 @@ public class MainActivity extends ActionBarActivity
             super.onAttach(activity);
             ((MainActivity) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+    // REST
+    private static final String ENDERECO = "http://192.168.0.7:8080/Rest/Servico/get?email=";
+
+    private void buscarTreino() {
+        new HttpAsyncTask().execute(ENDERECO + buscarEmail());
+    }
+
+    private String buscarEmail() {
+        USuarioHelper userDAO = new USuarioHelper(getApplication());
+        return userDAO.buscarTodos().get(0).getEmail();
+    }
+
+    /**
+     * Metodo para receber GET
+     * @param url
+     * @return
+     */
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null) {
+                result = convertInputStreamToString(inputStream);
+            } else {
+                result = "Did not work!";
+            }
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+        return result;
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+            JSONArray jsonArray = null;
+            try {
+                jsonArray = new JSONArray(result);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (jsonArray != null) {
+                    JSONObject jsonObject = null;
+                   List<Treino> treinos = new ArrayList<Treino>();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        jsonObject = jsonArray.getJSONObject(i);
+                        Treino treino = new Treino();
+                        treino.setNome(jsonObject.getString("nome"));
+                        treino.setRepeticao(jsonObject.getInt("repeticao"));
+                        treino.setSequencia(jsonObject.getInt("sequencia"));
+                        treino.setDescricao(jsonObject.getString("descricao"));
+                        treinos.add(treino);
+                    }
+                    StringBuilder sb = new StringBuilder();
+                    String nomeTreino = "";
+                    for (Treino t : treinos) {
+                        if (!t.getNome().equals(nomeTreino)) {
+                            nomeTreino = t.getNome();
+                            sb.append("\n");
+                            sb.append(t.getNome());
+                            sb.append("\n");
+                        }
+                        sb.append(t.getDescricao());
+                        sb.append(" ");
+                        sb.append(t.getRepeticao());
+                        sb.append("x de ");
+                        sb.append(t.getSequencia());
+                        sb.append("\n");
+                    }
+                    treinoTextView = (TextView) findViewById(R.id.textTreino);
+                    if (sb.length() > 0) {
+                        treinoTextView.setText(sb.toString());
+                    } else {
+                        treinoTextView.setText("Sem treino para o momento.");
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
---
+++ app/src/main/java/meutreino/com/br/meutreino/entidade/Treino.java
@@ -0,0 +1,47 @@
+package meutreino.com.br.meutreino.entidade;
+
+import java.io.Serializable;
+
+/**
+ * Created by thiago on 22/06/15.
+ */
+public class Treino implements Serializable {
+
+    private int repeticao;
+    private String nome;
+    private int sequencia;
+    private String descricao;
+
+
+    public int getRepeticao() {
+        return repeticao;
+    }
+
+    public void setRepeticao(int repeticao) {
+        this.repeticao = repeticao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSequencia() {
+        return sequencia;
+    }
+
+    public void setSequencia(int sequencia) {
+        this.sequencia = sequencia;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
